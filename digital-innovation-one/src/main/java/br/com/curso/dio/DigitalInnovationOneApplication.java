37db37f7501e637bb41c4171df93a734054e702c
Alessandro <alessandro.mpgh@gmail.com>
2021-06-30 23:20:27 -0300

    Project

---
+++ digital-innovation-one/src/main/java/br/com/curso/dio/DigitalInnovationOneApplication.java
@@ -0,0 +1,14 @@
+package br.com.curso.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalInnovationOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalInnovationOneApplication.class, args);
+	}
+
+}
+
